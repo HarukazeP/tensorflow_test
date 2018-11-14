56dc0a529abbe79ddd9a509e18668802bd5769a6
tamakiHaru <pj2d3woa@s.okayama-u.ac.jp>
2018-11-14 17:59:42 +0900

    try change vocab

--- Programs/2018_from08to11/seq2seq_attention_change_vocab.py
+++ Programs/2018_from08to11/seq2seq_attention_change_vocab.py
@@ -1330,6 +1330,7 @@ if __name__ == '__main__':
         #テストデータに対する予測と精度の計算
         #選択肢を使ったテスト
         #これは前からの予測
+        print(vocab_path)
         print('center')
         test_choices(vocab, my_encoder, my_decoder, center_data, center_choices, saveAttention=False, file_output=True)
 
