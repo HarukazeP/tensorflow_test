64f32f8f7e940246368c71ff1e9f549287bcd1f5
tamakiHaru <pj2d3woa@s.okayama-u.ac.jp>
2018-11-20 15:41:15 +0900

    update

--- Programs/2018_from08to11/lstm_seq2seq_keras.py
+++ Programs/2018_from08to11/lstm_seq2seq_keras.py
@@ -237,7 +237,7 @@ if args.mode == 'all' or args.mode == 'mini':
               epochs=epochs,
               validation_split=0.2)
     # Save model
-    model.save('s2s_keras_'+args.mode+'.h5')
+    model.save('/home/ohtalab/tamaki/M2/s2s_keras_'+args.mode+'_ep'+str(epochs)'.h5')
 
 else :
     print('load model')
