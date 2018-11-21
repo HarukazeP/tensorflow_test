cbba38a383c76c14f7bf073ee49912e6aba549e6
tamakiHaru <pj2d3woa@s.okayama-u.ac.jp>
2018-11-21 12:08:25 +0900

    update

--- Programs/2018_from08to11/lstm_seq2seq_keras_non_save.py
+++ Programs/2018_from08to11/lstm_seq2seq_keras_non_save.py
@@ -4,6 +4,7 @@
 kerasを用いたseq2seqモデル
 急遽実装
 kerasチュートリアル→先生が編集→自分でテスト部分追加
+711で動かすように古いkerasの書き方に変更
 
 
 Sequence to sequence example in Keras (character-level).
@@ -208,7 +209,7 @@ for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
 
 # Define an input sequence and process it.
 encoder_inputs = Input(shape=(None, num_encoder_tokens))
-encoder = LSTM(latent_dim, return_state=True)
+encoder = LSTM(latent_dim)
 encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 # We discard `encoder_outputs` and only keep the states.
 encoder_states = [state_h, state_c]
@@ -218,7 +219,7 @@ decoder_inputs = Input(shape=(None, num_decoder_tokens))
 # We set up our decoder to return full output sequences,
 # and to return internal states as well. We don't use the
 # return states in the training model, but we will use them in inference.
-decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_lstm = LSTM(latent_dim, return_sequences=True)
 decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
                                      initial_state=encoder_states)
 decoder_dense = Dense(num_decoder_tokens, activation='softmax')
