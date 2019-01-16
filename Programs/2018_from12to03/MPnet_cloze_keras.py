06bd931310761de0df3e38fc8bc769fa5a999b4c
tamakiHaru <pj2d3woa@s.okayama-u.ac.jp>
2019-01-16 20:40:26 +0900

    update

--- Programs/2018_from12to03/MPnet_cloze_keras.py
+++ Programs/2018_from12to03/MPnet_cloze_keras.py
@@ -747,7 +747,7 @@ class Ngram():
 
 
 #学習をn_iters回，残り時間の算出をlossグラフの描画も
-def trainIters(Ngram, lang, model, train_pairs, val_pairs, n_iters, print_every=10, learning_rate=0.001, saveModel=False):
+def trainIters(ngram, lang, model, train_pairs, val_pairs, n_iters, print_every=10, learning_rate=0.001, saveModel=False):
 
     X_train_tmp=np.array([sent_to_ids_cloze(lang, s) for s in train_pairs[0]], dtype=np.int)
     C_train=np.array([choices_to_ids(lang, s) for s in train_pairs[1]], dtype=np.int)
@@ -765,14 +765,30 @@ def trainIters(Ngram, lang, model, train_pairs, val_pairs, n_iters, print_every=
     cloze_val=make_cloze_onehot(X_val_tmp)
 
     # MPALayerの一部: N-gram Statistics 用の入力
-    N1_train, N2_train, N3_train, N4_train=Ngram.get_ngram_count(train_pairs[0], train_pairs[1])
-    N1_val, N2_val, N3_val, N4_val=Ngram.get_ngram_count(val_pairs[0], val_pairs[1])
+    N1_train, N2_train, N3_train, N4_train=ngram.get_ngram_count(train_pairs[0], train_pairs[1])
+    N1_val, N2_val, N3_val, N4_val=ngram.get_ngram_count(val_pairs[0], val_pairs[1])
 
     # 自作拡張: 空所補充文Attentive Reader 用の入力
     # TODO:
 
     # 自作拡張: KenLM Score 用の入力
     # TODO:
+
+    if args.model_kind=='origin':
+        pass
+    elif args.model_kind=='plus_CAR':
+        pass
+    elif args.model_kind=='plus_KenLM':
+        pass
+    elif args.model_kind=='plus_both':
+        pass
+    elif args.model_kind=='replace_CAR':
+        pass
+    elif args.model_kind=='replace_KenLM':
+        pass
+    elif args.model_kind=='replace_both':
+        pass
+
 
     X_train=[X_train_tmp, c1_train, c2_train, c3_train, c4_train, cloze_train, N1_train, N2_train, N3_train, N4_train]
     X_val=[X_val_tmp, c1_val, c2_val, c3_val, c4_val, cloze_val, N1_val, N2_val, N3_val, N4_val]
@@ -839,172 +855,62 @@ def showPlot4(val_plot, file_name, label_name):
 ###########################
 # 4.モデルによる予測
 ###########################
-'''
-#TODO テスト部分後日実装
+def model_test(ngram, lang, model, cloze_path, choices_path, ans_path, data_name, file_output=True):
+    '''
+    ファイル読み込み
+    save_pathの一時書き換えというか，新規パスに保存
+    model.evaluate使う
+    '''
+    print(data_name)
 
-    # 1データに対する予測
-    def evaluate(lang, model, sentence, max_length=MAX_LENGTH):
-        with torch.no_grad():
-            #no_grad()の間はパラメータが固定される（更新されない）
-            input_indexes = pad_indexes(lang, sentence)
-            input_batch = torch.tensor([input_indexes], dtype=torch.long, device=my_device)  # (1, s)
+    test_X=readCloze(cloze_path)
+    test_C=readChoices(choices_path)
+    test_Y=readAns(ans_path)
 
-            encoder_outputs, encoder_hidden = encoder(input_batch.transpose(0, 1))
+    X_test_tmp=np.array([sent_to_ids_cloze(lang, s) for s in test_X], dtype=np.int)
+    C_test=np.array([choices_to_ids(lang, s) for s in test_C], dtype=np.int)
+    Y_test=np.array([ans_to_ids(lang, s, c) for s,c in zip(test_Y, test_C)], dtype=np.bool)
 
-            decoder_input = torch.tensor([SOS_token], device=my_device)  # SOS
-            decoder_hidden = (
-                (encoder_hidden[0][0].squeeze(0), encoder_hidden[1][0].squeeze(0)),
-                (encoder_hidden[0][1].squeeze(0), encoder_hidden[1][1].squeeze(0))
-                )
+    c1_test, c2_test, c3_test, c4_test = split_choices(C_test)
 
-            decoded_words = []
-            decoder_attentions = []
+    # MPALayerの一部: Selective Copying 用の入力
+    cloze_test=make_cloze_onehot(X_test_tmp)
 
-            for di in range(max_length):
-                decoder_output, decoder_hidden, attention = decoder(decoder_input, decoder_hidden, encoder_outputs)  # (1,outdim), ((1,h),(1,h)), (l,1)
-                decoder_attentions.append(attention)
-                _, topi = decoder_output.topk(1)  # (1, 1)
-                if topi.item() == EOS_token:
-                    decoded_words.append('<EOS>')
-                    break
-                else:
-                    decoded_words.append(lang.index2word[topi.item()])
+    # MPALayerの一部: N-gram Statistics 用の入力
+    N1_test, N2_test, N3_test, N4_test=ngram.get_ngram_count(test_X, C_test)
 
-                decoder_input = topi[0]
+    # 自作拡張: 空所補充文Attentive Reader 用の入力
+    # TODO:
 
-            decoder_attentions = torch.cat(decoder_attentions, dim=0)  # (l, n)
-
-            #返り値は予測した単語列とattentionの重み？
-            return decoded_words, decoder_attentions.squeeze(0)
+    # 自作拡張: KenLM Score 用の入力
+    # TODO:
 
 
-    #attentionの重みの対応グラフの描画
-    def showAttention(file_header, input_sentence, output_words, attentions):
-        #TODO 描画方法は要改善
-        #目盛り間隔、軸ラベルの位置など
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        cax = ax.matshow(attentions.numpy().T, cmap='bone')
-        fig.colorbar(cax)
-
-        ax.set_yticklabels([''] + input_sentence.split(' ') +
-                           ['<EOS>'])
-        ax.set_xticklabels([''] + output_words, rotation=90)
-
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-        ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
-        if len(input_sentence)>10:
-            plt.savefig(save_path + file_header + input_sentence[:10] + '_attn.png')
-        else:
-            plt.savefig(save_path + file_header + input_sentence + '_attn.png')
+    if args.model_kind=='origin':
+        pass
+    elif args.model_kind=='plus_CAR':
+        pass
+    elif args.model_kind=='plus_KenLM':
+        pass
+    elif args.model_kind=='plus_both':
+        pass
+    elif args.model_kind=='replace_CAR':
+        pass
+    elif args.model_kind=='replace_KenLM':
+        pass
+    elif args.model_kind=='replace_both':
+        pass
 
 
-    #精度いろいろ計算
-    #問題文、完全一致文、空所の完答文、空所の一部正答文、BLEU値、空所ミス文
-    def calc_score(preds_sentences, ans_sentences):
-        line_num=0
-        allOK=0
-        clozeOK=0
-        partOK=0
-        miss=0
-        BLEU=0
+    X_test=[X_test_tmp, c1_test, c2_test, c3_test, c4_test, cloze_test, N1_test, N2_test, N3_test, N4_test]
 
-        for pred, ans in zip(preds_sentences, ans_sentences):
-            pred=pred.replace(' <EOS>', '')
-            flag=0
-            if pred == ans:
-                allOK+=1
-                flag=1
-            pred_cloze = get_cloze(pred)
-            ans_cloze = get_cloze(ans)
-            tmp_ans_length=len(ans_cloze.split(' '))
-            line_num+=1
-            if is_correct_cloze(pred):
-                tmp_match=match(pred_cloze, ans_cloze)
-                if tmp_match > 0:
-                    partOK+=1
-                if pred_cloze == ans_cloze:
-                    clozeOK+=1
-                    if flag==0:
-                        print(pred)
-                        print(ans)
-            else:
-                miss+=1
+    loss, acc=model.evaluate(X_test, Y_test, batch_size=BATCH_SIZE, verbose=1)
+    print('loss=%.4f, acc=%.2f' % (loss, acc))
 
-        #BLEU=compute_bleu(preds_sentences, ans_sentences)
+    if file_output:
+        with open(save_path+data_name+'_result.txt'+, 'w') as f:
+            f.write('loss=%.4f, acc=%.2f' % (loss, acc))
 
-        return line_num, allOK, clozeOK, partOK, BLEU, miss
-
-
-    def output_preds(file_name, preds):
-        with open(file_name, 'w') as f:
-            for p in preds:
-                f.write(p+'\n')
-
-
-    def print_score(line, allOK, clozeOK, partOK, BLEU, miss):
-        print('  acc(all): ', '{0:.2f}'.format(1.0*allOK/line*100),' %')
-        #print('acc(cloze): ', '{0:.2f}'.format(1.0*clozeOK/line*100),' %')
-        #print(' acc(part): ', '{0:.2f}'.format(1.0*partOK/line*100),' %')
-
-        #print(' BLEU: ','{0:.2f}'.format(BLEU*100.0))
-        print('  all: ', allOK)
-        #print('cloze: ',clozeOK)
-        #print(' part: ',partOK)
-        print(' line: ',line)
-        print(' miss: ',miss)
-
-
-    def output_score(file_name, line, allOK, clozeOK, partOK, BLEU, miss):
-        output=''
-        output=output+'  acc(all): '+str(1.0*allOK/line*100)+' %\n'
-        #output=output+'acc(cloze): '+str(1.0*clozeOK/line*100)+' %\n'
-        #output=output+' acc(part): '+str(1.0*partOK/line*100)+' %\n\n'
-        #output=output+'      BLEU: '+str(BLEU*100.0)+' %\n\n'
-        output=output+'       all: '+str(allOK)+'\n'
-        #output=output+'     cloze: '+str(clozeOK)+'\n'
-        #output=output+'      part: '+str(partOK)+'\n'
-        output=output+'      line: '+str(line)+'\n'
-        output=output+'      miss: '+str(miss)+'\n'
-
-        with open(file_name, 'w') as f:
-            f.write(output)
-
-
-    def score(preds, ans, file_output, file_name):
-        #精度のprintとファイル出力
-        line, allOK, clozeOK, partOK, BLEU, miss = calc_score(preds, ans)
-        print_score(line, allOK, clozeOK, partOK, BLEU, miss)
-        if file_output:
-            output_score(file_name, line, allOK, clozeOK, partOK, BLEU, miss)
-
-
-    #テストデータに対する予測と精度計算
-    #空所内のみを予測するモード
-    #および、選択肢を利用するモード
-    def test_choices(lang, model, test_data, choices, saveAttention=False, file_output=False):
-        print("Test ...")
-        #input_sentence や ansは文字列であるのに対し、output_wordsはリストであることに注意
-        preds=[]
-        ans=[]
-        for pair, choi in zip(test_data, choices):
-            input_sentence=pair[0]
-            ans.append(pair[1])
-
-            output_words, attentions = evaluate(lang, model, input_sentence)
-            preds.append(' '.join(output_words))
-
-            if saveAttention:
-                showAttention('all', input_sentence, output_words, attentions)
-
-            if file_output:
-                output_preds(save_path+'preds.txt', preds)
-
-        print("Calc scores ...")
-        score(preds, ans, file_output, save_path+'score.txt')
-
-'''
 
 #コマンドライン引数の設定いろいろ
 def get_args():
@@ -1014,7 +920,9 @@ def get_args():
     parser.add_argument('--model_dir', help='model directory path (when load model, mode=test)')
     parser.add_argument('--model', help='model file name (when load model, mode=test)')
     parser.add_argument('--epoch', type=int, default=30)
-    #TODO ほかにも引数必要に応じて追加
+    parser.add_argument('--model_kind',, choices=['origin', 'plus_CAR', 'plus_KenLM', 'plus_both', 'replace_CAR', 'replace_KenLM', 'replace_both'], default='origin', help='model file kind')
+
+    # ほかにも引数必要に応じて追加
     return parser.parse_args()
 
 
@@ -1037,6 +945,20 @@ if __name__ == '__main__':
 
     #学習時
     if args.mode == 'all' or args.mode == 'mini':
+        if args.model_kind=='origin':
+            pass
+        elif args.model_kind=='plus_CAR':
+            pass
+        elif args.model_kind=='plus_KenLM':
+            pass
+        elif args.model_kind=='plus_both':
+            pass
+        elif args.model_kind=='replace_CAR':
+            pass
+        elif args.model_kind=='replace_KenLM':
+            pass
+        elif args.model_kind=='replace_both':
+            pass
         model = build_model(vocab.n_words, EMB_DIM, HIDDEN_DIM, weights_matrix)
 
         train_cloze=CLOTH_path+'CLOTH_train_cloze.txt'
@@ -1083,6 +1005,7 @@ if __name__ == '__main__':
 
         # 3.学習
         model = trainIters(clothNg, vocab, model, train_data, val_data, n_iters=epoch, saveModel=True)
+        print('Train end')
 
     #すでにあるモデルでテスト時
     else:
@@ -1097,71 +1020,32 @@ if __name__ == '__main__':
         save_path=save_path+today_str
 
     # 4.評価
-    print('Train end')
-
-    '''
-    #TODO テスト未実装
-
     center_cloze=git_data_path+'center_cloze.txt'
-    center_ans=git_data_path+'center_ans.txt'
     center_choi=git_data_path+'center_choices.txt'
+    center_ans=git_data_path+'center_ans.txt'
 
     MS_cloze=git_data_path+'microsoft_cloze.txt'
-    MS_ans=git_data_path+'microsoft_ans.txt'
     MS_choi=git_data_path+'microsoft_choices.txt'
+    MS_ans=git_data_path+'microsoft_ans.txt'
 
     high_path=git_data_path+'CLOTH_test_high'
     middle_path=git_data_path+'CLOTH_test_middle'
 
     CLOTH_high_cloze = high_path+'_cloze.txt'
-    CLOTH_high_ans = high_path+'_ans.txt'
     CLOTH_high_choi = high_path+'_choices.txt'
+    CLOTH_high_ans = high_path+'_ans.txt'
 
     CLOTH_middle_cloze = middle_path+'_cloze.txt'
-    CLOTH_middle_ans = middle_path+'_ans.txt'
     CLOTH_middle_choi = middle_path+'_choices.txt'
+    CLOTH_middle_ans = middle_path+'_ans.txt'
 
-    print("Reading test data...")
-    center_X=readCloze(center_cloze)
-    center_C=readChoices(center_choices)
-    center_Y=readAns(center_ans)
+    is_out=False    #ファイル出力一括設定用
 
-    MS_X=readCloze(MS_cloze)
-    MS_C=readChoices(MS_choices)
-    MS_Y=readAns(MS_ans)
+    model_test(clothNg, vocab, center_cloze, center_choi, center_ans, data_name='center', file_output=is_out)
 
-    CLOTH_high_X=readCloze(CLOTH_high_cloze)
-    CLOTH_high_C=readChoices(CLOTH_high_choices)
-    CLOTH_high_Y=readAns(CLOTH_high_ans)
+    if args.mode != 'mini':
+        model_test(clothNg, vocab, MS_cloze, MS_choi, MS_ans, data_name='MS', file_output=is_out)
 
-    CLOTH_middle_X=readCloze(CLOTH_middle_cloze)
-    CLOTH_middle_C=readChoices(CLOTH_middle_choices)
-    CLOTH_middle_Y=readAns(CLOTH_middle_ans)
+        model_test(clothNg, vocab, CLOTH_high_cloze, CLOTH_high_choi, CLOTH_high_ans, data_name='CLOTH_high', file_output=is_out)
 
-    if args.mode == 'mini' or args.mode == 'mini_test':
-        center_data=center_data[:5]
-        center_choices=center_choices[:5]
-        MS_data=MS_data[:5]
-        MS_choices=MS_choices[:5]
-
-
-    #テストデータに対する予測と精度の計算
-    #選択肢を使ったテスト
-    #これは前からの予測
-    print('center')
-    tmp=save_path
-    save_path=tmp+'center_'
-    test_choices(vocab, my_encoder, my_decoder, center_data, center_choices, saveAttention=False, file_output=True)
-
-    #これは文スコア
-    test_choices_by_sent_score(vocab, my_encoder, my_decoder, center_data, center_choices, saveAttention=False, file_output=False)
-
-    print('MS')
-    tmp=save_path
-    save_path=tmp+'MS_'
-    test_choices(vocab, my_encoder, my_decoder, MS_data, MS_choices, saveAttention=False, file_output=True)
-
-    #これは文スコア
-    test_choices_by_sent_score(vocab, my_encoder, my_decoder, MS_data, MS_choices, saveAttention=False, file_output=False)
-
-    '''
+        model_test(clothNg, vocab, CLOTH_middle_cloze, CLOTH_middle_choi, CLOTH_middle_ans, data_name='CLOTH_middle', file_output=is_out)
